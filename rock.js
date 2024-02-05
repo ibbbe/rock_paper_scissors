b3b56526f98f7acdc31a2b37280fd28f54f704d8
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-05 18:24:04 +0100

    Add getComputerChoice function
    getComputerChoice function simulates computer game choice.
    
    Changes to be committed:
    	modified:   rock.js

---
+++ rock.js
@@ -0,0 +1,8 @@
+
+// Function to get computer choice between 1 and 3
+let computerChoice = function getComputerChoice() {
+    return Math.floor(Math.random() *3);
+}
+
+// console.log(computerChoice());
+
