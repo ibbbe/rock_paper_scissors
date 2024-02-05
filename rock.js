e5b21f63c0485f593846e8d0b0fa1419aad36a1c
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-05 18:53:08 +0100

    Add playerSelection
    Changed the name of computerChoice to computerSelection.
    Created variable playerSelection to get player choice input.
    
    Changes to be committed:
    	modified:   rock.js

--- rock.js
+++ rock.js
@@ -1,8 +1,10 @@
 
 // Function to get computer choice between 1 and 3
-let computerChoice = function getComputerChoice() {
-    return Math.floor(Math.random() *3);
+let computerSelection = function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
 }
 
 // console.log(computerChoice());
+
+let playerSelection = prompt("Please enter your choice:\nFor rock input 0,\npaper input 1,\nand scissor input 2: ", "");
 
