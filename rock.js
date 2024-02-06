007fd251828d93c274170b42363c80bd4bb1b3b8
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-06 09:48:51 +0100

    Add function playRound
    The function can play a single round of game. Computer choose
    from array, and the user input his choice through prompt.
    playRound function takes playerSelection and computerSelection
    decides and return the result of the game.
    
    Changes to be committed:
    	modified:   rock.js

--- rock.js
+++ rock.js
@@ -1,10 +1,66 @@
 
 // Function to get computer choice between 1 and 3
-let computerSelection = function getComputerChoice() {
+function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-// console.log(computerChoice());
+const choiceArray = ["rock", "paper", "scissors"];
+let computerSelection = choiceArray[getComputerChoice()];
+// console.log(computerSelection);
 
-let playerSelection = prompt("Please enter your choice:\nFor rock input 0,\npaper input 1,\nand scissor input 2: ", "");
+// Function that get's player choice and change the type from string to integer
+let playerSelection = prompt("Please enter your choice: ", "");
+playerSelection = playerSelection.toLowerCase();
+
+// console.log(playerSelection);
+
+let gameResult;
+
+console.log(gameResult);
+function playRound(playerSelection, computerSelection) {
+    
+    if (computerSelection == "rock") {
+        switch (playerSelection) {
+            case "rock":
+                return gameResult = "draw";
+                break;
+            case "paper":
+                return gameResult = "win";
+                break;
+            case "scissors":
+                return "loose";
+                break;
+        }
+    } else if(computerSelection == "paper") {
+        switch (playerSelection) {
+            case "rock":
+                return gameResult = "loose";
+                break;
+            case "paper":
+                return gameResult = "draw";
+                break;
+            case "scissors":
+                return gameResult = "win";
+                break;
+        }
+    } else if(computerSelection == "scissors") {
+        switch (playerSelection) {
+            case "rock":
+                return gameResult = "win";
+                break;
+            case "paper":
+                return gameResult = "loose";
+                break;
+            case "scissors":
+                return gameResult = "draw";
+                break;
+        }
+        return gameResult;
+        // console.log(gameResult);
+    }
+}
+
+// console.log(playRound(playerSelection, computerSelection));
+
+
 
