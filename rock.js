24a9c20747692d1bc8b745d0903a4f162e413959
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-08 10:00:31 +0100

    Add function playGame()
    Wrapped all in function playGame(), usedwhile loop
    to play 5 rounds, and display the winner of the
    game.
    
    Renamed computerResult to computerScore, and
    playerResult to playerScore, for better clarity of
    the code. Put increment on it to count the result.
    
     Changes to be committed:
    	modified:   rock.js

--- rock.js
+++ rock.js
@@ -1,66 +1,87 @@
 
-// Function to get computer choice between 1 and 3
-function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
-}
+function playGame() {
 
-const choiceArray = ["rock", "paper", "scissors"];
-let computerSelection = choiceArray[getComputerChoice()];
-// console.log(computerSelection);
+    let counter = 0;
+    let computerScore = 0;
+    let playerScore = 0;
 
-// Function that get's player choice and change the type from string to integer
-let playerSelection = prompt("Please enter your choice: ", "");
-playerSelection = playerSelection.toLowerCase();
+    // loop to repeat function calls 5 time(5 rounds of game)
+    while (counter < 5) {
+        counter++;
 
-// console.log(playerSelection);
-
-let gameResult;
-
-console.log(gameResult);
-function playRound(playerSelection, computerSelection) {
-    
-    if (computerSelection == "rock") {
-        switch (playerSelection) {
-            case "rock":
-                return gameResult = "draw";
-                break;
-            case "paper":
-                return gameResult = "win";
-                break;
-            case "scissors":
-                return "loose";
-                break;
+        // function to randomly get number from 0 to 2, to use as a index from choiceArray
+        function getComputerChoice() {
+            return Math.floor(Math.random() * 3);
         }
-    } else if(computerSelection == "paper") {
-        switch (playerSelection) {
-            case "rock":
-                return gameResult = "loose";
-                break;
-            case "paper":
-                return gameResult = "draw";
-                break;
-            case "scissors":
-                return gameResult = "win";
-                break;
+
+        const choiceArray = ["rock", "paper", "scissors"];
+        let computerSelection = choiceArray[getComputerChoice()];
+        console.log(computerSelection);
+
+        let playerSelection = prompt("Please enter your choice: ", "");
+        playerSelection = playerSelection.toLowerCase();
+
+        console.log(playerSelection);
+
+        // conditional block that verify the score(increment it) of one game round
+        if (computerSelection == "rock") {
+            switch (playerSelection) {
+                case "rock":
+                    console.log("It is a draw. Rock = Rock!")
+                    break;
+                case "paper":
+                    console.log("You win! Paper beats rock!")
+                    playerScore++;
+                    break;
+                case "scissors":
+                    console.log("You loose! Rock beats scissors!")
+                    computerScore++;
+                    break;
+            }
+        } else if (computerSelection == "paper") {
+            switch (playerSelection) {
+                case "rock":
+                    console.log("You loose! Paper beats rock!");
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log("It is a draw. Paper = Paper!");
+                    break;
+                case "scissors":
+                    console.log("You win! Scissors beats paper!");
+                    playerScore++;
+                    break;
+            }
+        } else if (computerSelection == "scissors") {
+            switch (playerSelection) {
+                case "rock":
+                    console.log("You win! Rock beats scissors!");
+                    playerScore++;
+                    break;
+                case "paper":
+                    console.log("You loose! Scissors beats paper!");
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log("It is a draw. Scissors = Scissors!")
+                    break;
+            }
+
+
         }
-    } else if(computerSelection == "scissors") {
-        switch (playerSelection) {
-            case "rock":
-                return gameResult = "win";
-                break;
-            case "paper":
-                return gameResult = "loose";
-                break;
-            case "scissors":
-                return gameResult = "draw";
-                break;
-        }
-        return gameResult;
-        // console.log(gameResult);
+    }
+
+    // conditional block that chooses the winner and write out the game result 
+    if (computerScore > playerScore) {
+        console.log(`You loose! Computer ${computerScore} : ${playerScore}.`);
+    } else if (computerScore < playerScore) {
+        console.log(`You win! ${playerScore} : ${computerScore} `);
+    } else {
+        console.log(`It is a draw ${computerScore} : ${playerScore}`);
     }
 }
 
-// console.log(playRound(playerSelection, computerSelection));
+playGame();
 
 
 
