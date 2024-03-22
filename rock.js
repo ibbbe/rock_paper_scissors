fae71109ba2dfe8ba34bbd086258f4bdfc30cf40
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-03-22 14:11:18 +0100

    Finished Rock paper scissors
    Removed the logic that played exactly five rounds.
    Created three html buttons(rock, paper, scissors).
    Added a div for displaying current, and another
    div to display final result.
    Changed all console.log outputs into Dom methods.
    
    Wrapped it to stop the game when one of
    players get 5 wins.
    
    On branch rps-ui
    Changes to be committed:
    modified:   index.html
    modified:   rock.js

--- rock.js
+++ rock.js
@@ -1,13 +1,18 @@
 
-function playGame() {
+const buttons = document.querySelectorAll('button');
 
-    let counter = 0;
-    let computerScore = 0;
-    let playerScore = 0;
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        playGame(e.target.id);
+    })
+});
 
-    // loop to repeat function calls 5 time(5 rounds of game)
-    while (counter < 5) {
-        counter++;
+let computerScore = 0;
+let playerScore = 0;
+
+function playGame(playerInput) {
+
+    if (computerScore < 5 & playerScore < 5) {
 
         // function to randomly get number from 0 to 2, to use as index from choiceArray
         function getComputerChoice() {
@@ -15,73 +20,80 @@ function playGame() {
         }
 
         const choiceArray = ["rock", "paper", "scissors"];
+
         let computerSelection = choiceArray[getComputerChoice()];
-        console.log(computerSelection);
 
-        let playerSelection = prompt("Please enter your choice: ", "");
-        playerSelection = playerSelection.toLowerCase();
+        let playerSelection = choiceArray[playerInput];
 
-        console.log(playerSelection);
+        let div = document.querySelector('div');
+        let paragraphResult = document.createElement('p');
 
-        // conditional block that verify the score(increment it) of one game round
         if (computerSelection == "rock") {
             switch (playerSelection) {
                 case "rock":
-                    console.log("It is a draw. Rock = Rock!")
+                    paragraphResult.textContent = "It is a draw. Rock = Rock!";
+                    div.appendChild(paragraphResult);
                     break;
                 case "paper":
-                    console.log("You win! Paper beats rock!")
+                    paragraphResult.textContent = "You win! Paper beats rock!";
+                    div.appendChild(paragraphResult);
                     playerScore++;
                     break;
                 case "scissors":
-                    console.log("You loose! Rock beats scissors!")
+                    paragraphResult.textContent = "You loose! Rock beats scissors!";
+                    div.appendChild(paragraphResult);
                     computerScore++;
                     break;
             }
         } else if (computerSelection == "paper") {
             switch (playerSelection) {
                 case "rock":
-                    console.log("You loose! Paper beats rock!");
+                    paragraphResult.textContent = "You loose! Paper beats rock!";
+                    div.appendChild(paragraphResult);
                     computerScore++;
                     break;
                 case "paper":
-                    console.log("It is a draw. Paper = Paper!");
+                    paragraphResult.textContent = "It is a draw. Paper = Paper!";
+                    div.appendChild(paragraphResult);
                     break;
                 case "scissors":
-                    console.log("You win! Scissors beats paper!");
+                    paragraphResult.textContent = "You win! Scissors beats paper!";
+                    div.appendChild(paragraphResult);
                     playerScore++;
                     break;
             }
         } else if (computerSelection == "scissors") {
             switch (playerSelection) {
                 case "rock":
-                    console.log("You win! Rock beats scissors!");
+                    paragraphResult.textContent = "You win! Rock beats scissors!";
+                    div.appendChild(paragraphResult);
                     playerScore++;
                     break;
                 case "paper":
-                    console.log("You loose! Scissors beats paper!");
+                    paragraphResult.textContent = "You loose! Scissors beats paper!";
+                    div.appendChild(paragraphResult);
                     computerScore++;
                     break;
                 case "scissors":
-                    console.log("It is a draw. Scissors = Scissors!")
+                    paragraphResult.textContent = "It is a draw. Scissors = Scissors!";
+                    div.appendChild(paragraphResult);
                     break;
             }
+        }
+        return computerScore;
+        return playerScore;
+    }
+    else {
+        const finalScoreDiv = document.querySelector('#finalResult');
+        const finalScoreP = document.createElement('p');
 
-
+        if (computerScore > playerScore) {
+            finalScoreP.textContent = `You loose! Computer wins ${computerScore} : ${playerScore}.`
+            finalScoreDiv.appendChild(finalScoreP);
+        } else {
+            (computerScore < playerScore)
+            finalScoreP.textContent = `You win! ${playerScore} : ${computerScore} `;
+            finalScoreDiv.appendChild(finalScoreP);
         }
     }
-
-    // conditional block that chooses the winner and write out the game result 
-    if (computerScore > playerScore) {
-        console.log(`You loose! Computer ${computerScore} : ${playerScore}.`);
-    } else if (computerScore < playerScore) {
-        console.log(`You win! ${playerScore} : ${computerScore} `);
-    } else {
-        console.log(`It is a draw ${computerScore} : ${playerScore}`);
-    }
 }
-
-playGame();
-
-
-
